42f4451017994cf2a1cc60dd44619b02bbe24800
azzamaflah <azzamhebat46@gmail.com>
2024-10-12 01:31:41 +0700

    Menampilkan list baca-saja dengan `listOf` Menambahkan elemen, menghapus, dan mengganti elemen pada`MutableList`

---
+++ app/src/main/java/com/example/el/Collection.kt
@@ -0,0 +1,22 @@
+package com.example.el
+
+fun ContohList(){
+    println("=== List ===")
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    shape.add("Circle")
+    println(shape)
+
+    shape.remove("Triangle")
+    println(shape)
+
+    shape[0] = "Oval"
+    println(shape)
+
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
