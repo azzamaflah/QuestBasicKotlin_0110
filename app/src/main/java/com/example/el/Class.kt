e9b5cb40db7b8cc6267eb8bc40041333d195da4d
azzamaflah <azzamhebat46@gmail.com>
2024-10-13 01:22:40 +0700

    ini untuk menambahkan kelas motor dan kayak manipulasi objek nya si contact

---
+++ app/src/main/java/com/example/el/Class.kt
@@ -0,0 +1,19 @@
+package com.example.el
+
+
+
+class Motor()
+
+class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.email)
+
+    //update the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    // print the value
+    println(contact.email)
+}
