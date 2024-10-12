af1f2889536cc51aa2f02799e088b34f1f362679
azzamaflah <azzamhebat46@gmail.com>
2024-10-13 02:04:49 +0700

    Menambahkan contoh null safety dan nullable types di Kotlin

---
+++ app/src/main/java/com/example/el/NullSafety.kt
@@ -0,0 +1,25 @@
+package com.example.el
+
+fun nullSafety(){
+    var neverNull: String = "This cant be null"
+
+    var nullable: String? = "You can keep a null here"
+
+    nullable = null
+
+    if (neverNull == null){
+        println("inferredNoNull is null")
+    }
+    else{
+        println("inferredNoNull is not null")
+    }
+
+    println(neverNull.length)
+    println(nullable?.length)
+
+    fun strLength(notNull: String): Int{
+        return notNull.length
+    }
+
+    println(strLength(neverNull))
+}
