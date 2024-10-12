5ba1cb09595c458867ab6ca9937b921a4c6b196d
azzamaflah <azzamhebat46@gmail.com>
2024-10-13 01:42:57 +0700

    ini menamabahkan funsgi parameter terus ada yang pakai parameter dan nama argument

---
+++ app/src/main/java/com/example/el/Functions.kt
@@ -0,0 +1,17 @@
+fun withoutParameter(){
+    println("=== withoutParameter ===")
+    println("Hello, World")
+}
+
+fun withParameter(name: String){
+    println()
+    println("=== withParameter ===")
+    println("Hello, $name")
+}
+
+fun withNamedArgument(name : String, age: Int){
+    println()
+    println("=== withNamedArgument ===")
+    println("Hello, $name! You are $age years old.")
+}
+
